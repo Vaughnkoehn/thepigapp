d05df1e4d2f17811e8e8aff10290fd1f86c20602
Vaughn <Vkoehn99@gmail.com>
2018-01-15 21:49:16 -0600

    changed database engine

--- Thepigapp/settings.py
+++ Thepigapp/settings.py
@@ -78,13 +78,13 @@ WSGI_APPLICATION = 'Thepigapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-       # 'ENGINE': 'django.db.backends.mysql',
-       # 'NAME': 'Vaughn$default',
-       # 'USER': 'Vaughn',
-       # 'PASSWORD': 'vkccsscs',
-       # 'HOST': 'Vaughn.mysql.pythonanywhere-services.com',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Vaughn$default',
+        'USER': 'Vaughn',
+        'PASSWORD': 'vkccsscs',
+        'HOST': 'Vaughn.mysql.pythonanywhere-services.com',
     }
 }
 
